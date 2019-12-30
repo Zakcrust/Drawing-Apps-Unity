26b42b0827689b9e0e73fefb32d5c9e4d089a33b
zakcrust <zakcrusted@gmail.com>
2019-12-30 14:01:52 +0700

    Membuat Segi Empat

--- Assets/Scripts/Painter.cs
+++ Assets/Scripts/Painter.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class Painter : MonoBehaviour
 {
@@ -29,6 +30,34 @@ public class Painter : MonoBehaviour
 
     Vector3 lastPixelPosition;
     Vector3 startDownPos;
+    Vector3 lastMouseUpPos;
+
+    // Triangle variables
+    Vector3 startTrianglePos;
+    int lineCount = 0;
+    
+    public class ShapeModel
+    {
+        public DrawingMode Mode;
+        public List<Vector2> Vertices = new List<Vector2>();
+    }
+
+    public List<ShapeModel> ShapeModels = new List<ShapeModel>();
+
+    public ShapeModel currentDrawnShape;
+
+    public class Edge
+    {
+        public int x1, y1, x2, y2;
+        public Edge(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+    }
+
     void Start()
     {
         // Mendapatkan camera utama
@@ -103,9 +132,44 @@ public class Painter : MonoBehaviour
         lastPixelPosition = hit.textureCoord;
         tex.Apply(); */
          if (Input.GetMouseButtonDown(0))
-         {
-             startDownPos = pixelUV;
-         }
+        {
+            startDownPos = pixelUV;
+
+            switch (this.CurrentDrawingMode)
+            {
+                case DrawingMode.Line:
+                    currentDrawnShape = new ShapeModel();
+                    currentDrawnShape.Mode = DrawingMode.Line;
+                    break;
+
+               // ========= Fungsional membuat shape segitiga    ======//
+                case DrawingMode.Triangle:
+                    // posisi awal menekan mouse
+                    if (lineCount == 0)
+                    {
+                        currentDrawnShape = new ShapeModel();
+                        currentDrawnShape.Mode = DrawingMode.Triangle;
+
+                        startTrianglePos = startDownPos;
+
+                        // tambahkan data titik awal
+                        currentDrawnShape.Vertices.Add(pixelUV);
+                    }
+                    break;
+
+                case DrawingMode.Rectangle:
+                    currentDrawnShape = new ShapeModel();
+                    currentDrawnShape.Mode = DrawingMode.Rectangle;
+
+                    // data titik awal gambar segi empat
+                    currentDrawnShape.Vertices.Add(new Vector2(startDownPos.x, startDownPos.y));
+                    currentDrawnShape.Vertices.Add(new Vector2(pixelUV.x, startDownPos.y));
+                    currentDrawnShape.Vertices.Add(new Vector2(pixelUV.x, pixelUV.y));
+                    currentDrawnShape.Vertices.Add(new Vector2(startDownPos.x, pixelUV.y));
+                    break;
+
+            }
+        }
 
 
          if (Input.GetMouseButton(0))
@@ -125,6 +189,23 @@ public class Painter : MonoBehaviour
                         DrawBresenhamLine(ref tex, (int)startDownPos.x, (int)startDownPos.y, (int)pixelUV.x, (int)pixelUV.y);
                     }
                     break;
+
+                case DrawingMode.Triangle:
+                    tempTargetRender.gameObject.SetActive(true);
+
+                    if (lineCount == 0 && (startDownPos.x != pixelUV.x && startDownPos.y != pixelUV.y))
+                    {
+                        lastMouseUpPos = startDownPos;
+                        lineCount = 1;
+                        // tambahkan data titik baru
+                        currentDrawnShape.Vertices.Add(pixelUV);
+                    }
+
+                    if (currentDrawnShape.Vertices.Count > 0)
+                        currentDrawnShape.Vertices[currentDrawnShape.Vertices.Count - 1] = pixelUV;
+
+                    break;
+                
             }
         }
 
@@ -139,11 +220,114 @@ public class Painter : MonoBehaviour
                     targetTexture.Apply();
                     ClearColor(ref temporaryTexture);
                     break;
+             case DrawingMode.Triangle:
+
+                    // menghitung jumlah garis yang sudah digambar
+                    // indeks titik 0, 1, dan 2
+                    if (lineCount < 2)
+                    {
+
+                        if (lineCount == 0 && (startDownPos.x != pixelUV.x && startDownPos.y != pixelUV.y))
+                        {
+                            lineCount = 1;
+                        }
+                        else
+                        {
+                            lineCount++;
+
+                            // tambahkan data titik baru
+                            currentDrawnShape.Vertices.Add(pixelUV);
+                        }
+
+                    }
+                    else
+                    {
+
+                        // tambahkan data gambar
+                        ShapeModels.Add(currentDrawnShape);
+
+                        // reset indeks garis
+                        lineCount = 0;
+
+                        // reset data yang sedang digambar
+                        currentDrawnShape = null;
+                    }
+
+                    break;
+
+                case DrawingMode.Rectangle:
+
+                    currentDrawnShape.Vertices[0] = new Vector2(startDownPos.x, startDownPos.y);
+                    currentDrawnShape.Vertices[1] = new Vector2(pixelUV.x, startDownPos.y);
+                    currentDrawnShape.Vertices[2] = new Vector2(pixelUV.x, pixelUV.y);
+                    currentDrawnShape.Vertices[3] = new Vector2(startDownPos.x, pixelUV.y);
+
+                    // tambahkan data gambar persegi
+                    ShapeModels.Add(currentDrawnShape);
+
+                    // reset data yang sedang digambar
+                    currentDrawnShape = null;
+
+                    break;
             }
+            lastMouseUpPos = pixelUV;
+            
+            ClearColor(ref temporaryTexture);
+            RenderShapes(ref targetTexture);
+        }
+
+               // Preview update
+        if (currentDrawnShape != null && currentDrawnShape.Vertices.Count > 0)
+        {
+
+            Vector2 vertex1, vertex2;
+   
+            switch (this.CurrentDrawingMode)
+            {
+                case DrawingMode.Triangle:
+                    // titik yang sedang digerakkan
+                    currentDrawnShape.Vertices[currentDrawnShape.Vertices.Count - 1] = pixelUV;
+                    break;
+            }
+
+            // proses menggambar garis-garis preview
+            ClearColor(ref temporaryTexture);
+            for (int itVertex = 0; itVertex < currentDrawnShape.Vertices.Count - 1; itVertex++)
+            {
+                if (itVertex < currentDrawnShape.Vertices.Count - 1)
+                {
+                    vertex1 = currentDrawnShape.Vertices[itVertex];
+                    vertex2 = currentDrawnShape.Vertices[itVertex + 1];
+
+                    int x1 = (int)vertex1.x;
+                    int y1 = (int)vertex1.y;
+                    int x2 = (int)vertex2.x;
+                    int y2 = (int)vertex2.y;
+
+                    // garis penghubung
+                    DrawBresenhamLine(ref temporaryTexture, x1, y1, x2, y2);
+                }
+            }
+
+            switch (this.CurrentDrawingMode)
+            {
+                case DrawingMode.Rectangle:
+                    // garis penghubung terakhir untuk gambar segi empat
+                    vertex1 = currentDrawnShape.Vertices[currentDrawnShape.Vertices.Count - 1];
+                    vertex2 = currentDrawnShape.Vertices[0];
+                    int x1 = (int)vertex1.x;
+                    int y1 = (int)vertex1.y;
+                    int x2 = (int)vertex2.x;
+                    int y2 = (int)vertex2.y;
+                    DrawBresenhamLine(ref temporaryTexture, x1, y1, x2, y2);
+                    break;
+            }
+
         }
 
         lastPixelPosition = hit.textureCoord;
         tex.Apply();
+
     }
 
     void DrawDot(ref Texture2D targetTex, int x, int y)
@@ -168,7 +352,9 @@ public class Painter : MonoBehaviour
 
     public enum DrawingMode
     {
-        Line
+        Line,
+        Triangle,
+        Rectangle
     }
     public DrawingMode CurrentDrawingMode = DrawingMode.Line;
 
@@ -222,4 +408,76 @@ public class Painter : MonoBehaviour
         }
         targetTex.SetPixels(targetColors);
     }
+
+    public void RenderShapes(ref Texture2D texture)
+    {
+        ClearColor(ref texture);
+
+        int x1, y1, x2, y2;
+        Vector2 vertex1;
+        Vector2 vertex2;
+
+        List<Edge> edges = new List<Edge>();
+        for (int i = 0; i < this.ShapeModels.Count; i++)
+        {
+            ShapeModel imageModel = this.ShapeModels[i];
+            edges.Clear();
+
+            switch (imageModel.Mode)
+            {
+                case DrawingMode.Line:
+                    x1 = (int)imageModel.Vertices[0].x;
+                    y1 = (int)imageModel.Vertices[0].y;
+                    x2 = (int)imageModel.Vertices[1].x;
+                    y2 = (int)imageModel.Vertices[1].y;
+                    edges.Add(new Edge(x1, y1, x2, y2));
+                    break;
+
+// ================================
+// ========= Tambahkan proses hubungan antar titik
+// ================================
+                case DrawingMode.Rectangle:
+                case DrawingMode.Triangle:
+
+                    for (int itVertex = 0; itVertex < imageModel.Vertices.Count - 1; itVertex++)
+                    {
+                        if (itVertex < imageModel.Vertices.Count - 1)
+                        {
+                            vertex1 = imageModel.Vertices[itVertex];
+                            vertex2 = imageModel.Vertices[itVertex + 1];
+
+                            x1 = (int)vertex1.x;
+                            y1 = (int)vertex1.y;
+                            x2 = (int)vertex2.x;
+                            y2 = (int)vertex2.y;
+
+                            // garis penghubung
+                            edges.Add(new Edge(x1, y1, x2, y2));
+                        }
+                    }
+
+                    // garis terakhir
+                    vertex1 = imageModel.Vertices[imageModel.Vertices.Count - 1];
+                    vertex2 = imageModel.Vertices[0];
+                    x1 = (int)vertex1.x;
+                    y1 = (int)vertex1.y;
+                    x2 = (int)vertex2.x;
+                    y2 = (int)vertex2.y;
+                    edges.Add(new Edge(x1, y1, x2, y2));
+
+                    break;
+            }
+            
+           // gambar garis dari masing-masing edge
+            for (int itEdge = 0; itEdge < edges.Count; itEdge++)
+            {
+                Edge edge = edges[itEdge];
+                DrawBresenhamLine(ref texture, edge.x1, edge.y1, edge.x2, edge.y2);
+            }
+
+            texture.Apply();
+
+        }
+    }
+
 }
